a809ee4432735497c1c7bfd8d790fa921cd3170e
Jago <jago84@gmail.com>
2021-10-15 21:29:54 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'ui/message_list.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import '../data/message_dao.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        // TODO: Add ChangeNotifierProvider<UserDao> here
+        Provider<MessageDao>(lazy: false, create: (_) => MessageDao()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'RayChat',
+        theme: ThemeData(primaryColor: const Color(0xFF3D814A)),
+        // TODO: Add Consumer<UserDao> here
+        home: const MessageList(),
+      ),
+    );
+  }
+}
